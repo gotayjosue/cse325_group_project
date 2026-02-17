96e03777b066e3cedcf7f1bd475cfdb8e49be2db
gotayjosue <gotayjosue@gmail.com>
2026-02-17 00:30:03 -0600

    Adapted program.cs for Render deployment

--- Cse325GroupProject/Program.cs
+++ Cse325GroupProject/Program.cs
@@ -71,7 +71,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+
+//Adapted for Render deployment
+app.UseStatusCodePagesWithReExecute("/not-found");
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
